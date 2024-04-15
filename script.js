d5a3252be19fdbd7e2225bf12fbcdb694ef5e530
Leon Sostar <sostarleon@gmail.com>
2024-04-15 16:24:57 +0200

    Added opacity setting

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let opacity = 0;
 let userIn = 16;
 let picker = 'black';
 let heightWidth = 600 / userIn;
@@ -16,61 +17,75 @@ function getRandomRBG() {
 
 mainMan.addEventListener('click', (e) => {
     let valE = e.target;
-    if(userInn.value && Number(userInn.value) > 0 && Number(userInn.value) < 51 && valE.id !== 'inputEl') {
+    if (userInn.value && Number(userInn.value) > 0 && Number(userInn.value) < 51 && valE.id !== 'inputEl') {
         let val = Number(userInn.value);
         console.log(val);
 
         while (container.firstChild) {
             container.removeChild(container.firstChild);
         }
-        
-        heightWidth = 600/val;
+
+        heightWidth = 600 / val;
 
         for (let i = 0; i < val; i++) {
             let bigD = document.createElement('div');
             bigD.style.display = `flex`;
             container.appendChild(bigD);
-        
-            for (let i = 0; i < val; i++){
-            let div = document.createElement('div');
-            div.classList.add('highlight');
-            div.style.height = `${heightWidth}px`;
-            div.style.width = `${heightWidth}px`;
-            bigD.appendChild(div);
 
-            let target = e.target;
+            for (let i = 0; i < val; i++) {
+                let div = document.createElement('div');
+                div.classList.add('highlight');
+                div.style.height = `${heightWidth}px`;
+                div.style.width = `${heightWidth}px`;
+                bigD.appendChild(div);
 
-            if(target.id == "butt") {
+                let target = e.target;
 
-            div.addEventListener('mouseover', () => {
-                div.style.backgroundColor = 'black';
-            })
+                if (target.id == "butt") {
 
-            }
-            
-            if(target.id == "rainbow") {
+                    div.addEventListener('mouseover', () => {
+                        div.style.backgroundColor = 'black';
+                    })
 
-                div.addEventListener('mouseover', () => {
-                    div.style.backgroundColor = getRandomRBG();
-                })
-    
                 }
-            
+
+                if (target.id == "rainbow") {
+
+                    div.addEventListener('mouseover', () => {
+                        div.style.backgroundColor = getRandomRBG();
+                    })
+
+                }
+
+                if (target.id == "darkening") {
+
+                    div.addEventListener('mouseover', () => {
+                        div.style.backgroundColor = 'black';
+                        div.style.opacity = opacity;
+                        opacity += 0.1;
+                        if (opacity == 0.9999999999999999) {
+                            opacity = 0;
+                        }
+                        console.log(opacity);
+                    })
+
+                }
+
             }
-        
+
         }
 
     }
 
-    else if(valE.id == 'inputEl') {
-        
+    else if (valE.id == 'inputEl') {
+
     }
 
     else {
         alert('No negative values or numbers higher than 50 allowed!')
     }
-    
-    
+
+
 })
 
 
@@ -79,15 +94,15 @@ for (let i = 0; i < userIn; i++) {
     bigD.style.display = `flex`;
     container.appendChild(bigD);
 
-    for (let i = 0; i < userIn; i++){
-    let div = document.createElement('div');
-    div.classList.add('highlight');
-    div.style.height = `${heightWidth}px`;
-    div.style.width = `${heightWidth}px`;
-    bigD.appendChild(div);
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'black';
-    })
+    for (let i = 0; i < userIn; i++) {
+        let div = document.createElement('div');
+        div.classList.add('highlight');
+        div.style.height = `${heightWidth}px`;
+        div.style.width = `${heightWidth}px`;
+        bigD.appendChild(div);
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'black';
+        })
     }
 
 }
